6772745f53a935bd566c43dbf28f400149167bc0
isiss123 <abcecebab12@gmail.com>
2022-06-22 15:27:02 +0400

    ANGULAR Bolum-2 (Template ile calisma)

---
+++ Angular/2-Movie App/app/movie.ts
@@ -0,0 +1,8 @@
+export class Movie{
+    id;
+    name;
+    constructor(id: number,name: string){
+        this.id = id;
+        this.name = name;
+    }
+}
--- Angular/2-Movie App/app/movies/movies.component.ts
+++ Angular/2-Movie App/app/movies/movies.component.ts
@@ -1,10 +1,22 @@
 import { Component } from "@angular/core";
+import { Movie } from "../movie";
 
 @Component({
     selector:'movies', //<movies>...</movies>
-    templateUrl:'movies.component.html'
+    templateUrl:'movies.component.html',
+    styleUrls: ['./movies.component.css']
 })
 
 export class MoviesComponent{
-
+    title = 'Movies List'
+    movie: Movie = {
+        id:1,
+        name:'Axot 1'
+    }
+    getTitle(){
+        return this.title
+    }
+    getMovie(){
+        return this.movie
+    }
 }
