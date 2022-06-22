c90b92f91541b23264c0e3e7f5d06663fdfe82bd
isiss123 <abcecebab12@gmail.com>
2022-06-22 18:45:00 +0400

    ANGULAR Bolum-2 (Componente parametr gonderme(Inputs))

--- Angular/2-Movie App/app/app.module.ts
+++ Angular/2-Movie App/app/app.module.ts
@@ -5,13 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieComponent } from './movie/movie.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
-    MovieComponent
+    MovieComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule,
