867a74be5bf27717fda86782e9c9589bf4490e47
isiss123 <abcecebab12@gmail.com>
2022-06-28 16:38:29 +0400

    ANGULAR Bolum-3 ( Event Binding )

--- Angular/3-Data Binding/app/product.component.ts
+++ Angular/3-Data Binding/app/product.component.ts
@@ -9,27 +9,12 @@ import { ProductRepository } from "./models/repository.model";
 })
 export class ProductComponent{
     model: ProductRepository = new ProductRepository();
-    disabled = true;
-
-    color: string = "red";
-    getClasses(id: number): string{
-        let product = this.model.getProductById(id);
-        return (product.price <=2000 ? "bg-warning" : "bg-danger") + " p-2 m-2 text-white text-center"
+    onSubmit($event: any){
+        $event.stopPropagation(); // basqa click eventi gerceklestirme
+        console.log($event)
+        console.log("Button was clicked")
     }
-    getClassMap(id: number): Object{
-        let product = this.model.getProductById(id);
-        return {
-            "bg-primary": product.price<=1000,
-            "bg-black text-white": product.price>= 5000,
-            "text-center text-primary": product.name == "Axot 4"
-        }
+    onDiv(){
+        console.log("Div was clicked")
     }
-    getStyleMap(id: number): Object{
-        let product = this.model.getProductById(id);
-        return{
-            "font-size": "25px",
-            "color" : product.price <=2000 ? "black" : "pink"
-        }
-    }
-
 }
