17847bf36281110a05c0ac14de65cdd9718e80aa
isiss123 <abcecebab12@gmail.com>
2022-08-27 17:40:28 +0400

    ASP-Net Bolum-1 ( Static Files ) (Baslangic)

--- Asp-Net/Main/Program.cs
+++ Asp-Net/Main/Program.cs
@@ -1,11 +1,14 @@
+using Microsoft.Extensions.FileProviders;
+
 internal class Program
 {
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllersWithViews();
+        
         using var app = builder.Build();
-        app.UseStaticFiles();
+        
         app.UseRouting();
 
         // app.MapGet("/", () => "Hello World!");
@@ -15,6 +18,13 @@ internal class Program
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}"
             );
+        });
+        app.UseStaticFiles();
+        app.UseStaticFiles(new StaticFileOptions
+{
+            FileProvider = new PhysicalFileProvider(
+                Path.Combine(builder.Environment.ContentRootPath, "node_modules")),
+            RequestPath = "/node_modules"
         });
         app.Run();
 
