9d92ec22ec59ca9fdb07faa2cc8e5e5aebb041ea
isiss123 <abcecebab12@gmail.com>
2022-08-27 14:42:17 +0400

    ASP-Net Bolum-1 ( PartialView-e Dinamik melumat gonderme ) (Baslangic)

--- Asp-Net/Main/Controllers/HomeController.cs
+++ Asp-Net/Main/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
-
+using Main.Models;
+using Main.ViewModels;
 namespace Main.Controllers
 {
     public class HomeController : Controller
@@ -7,10 +8,24 @@ namespace Main.Controllers
         // localhost:7032/home/index
         public IActionResult Index()
         {
-            int saat = DateTime.Now.Hour;
-            ViewBag.Greetings = saat>12?"Xoş günlər":"Sabahınız xeir";
-            ViewBag.Username = "İstiqlal";
-            return View();
+            var products = new List<Product>(){
+                new Product(){Name="Axot 11",Price=11,Description="Bekar 11",IsApproved=false},
+                new Product(){Name="Axot 12",Price=12,Description="Bekar 12",IsApproved=true},
+                new Product(){Name="Axot 13",Price=13,Description="Bekar 13",IsApproved=true},
+                new Product(){Name="Axot 14",Price=14,Description="Bekar 14",IsApproved=false},
+                new Product(){Name="Axot 15",Price=15,Description="Bekar 15",IsApproved=true},
+                new Product(){Name="Axot 16",Price=16,Description="Bekar 16",IsApproved=true},
+                new Product(){Name="Axot 17",Price=17,Description="Bekar 17",IsApproved=false},
+            };
+            var categories = new List<Category>(){
+                new Category(){Name = "Yoxdu 11",Description="Yoxdu 11.1"},
+                new Category(){Name = "Yoxdu 12",Description="Yoxdu 12.1"},
+                new Category(){Name = "Yoxdu 13",Description="Yoxdu 13.1"},
+                new Category(){Name = "Yoxdu 14",Description="Yoxdu 14.1"},
+            };
+            
+            var ProductView = new ProductViewModel(){Products=products,Categories=categories};
+            return View(ProductView);
         }
         // localhost:7032/home/about
         public IActionResult About()
--- Asp-Net/Main/ViewModels/ProductViewModel.cs
+++ Asp-Net/Main/ViewModels/ProductViewModel.cs
@@ -5,13 +5,9 @@ namespace Main.ViewModels
     public class ProductViewModel
     {
         public List<Product> Products { get; set; }
-        public Category Category { get; set; }
+        public List<Category> Categories { get; set; }
         public ProductViewModel()
         {}
-        public ProductViewModel(List<Product> _products, Category _category)
-        {
-            this.Products = _products;
-            this.Category = _category;
-        }
+       
     }
 }
