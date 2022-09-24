49ab466a5aa6525cf2a2f8ccf16c5fc94633e60f
isiss123 <abcecebab12@gmail.com>
2022-09-24 14:19:23 +0400

    ASP-Net Bolum-11 ( Elave et butonlarinin hazirlanmasi ) (Alisveris sebeti)

---
+++ Asp-Net/Main/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Main.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddToCart()
+        {
+            return View();
+        }
+    }
+}
