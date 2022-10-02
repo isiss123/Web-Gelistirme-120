ba8b2673ad8eea7a82915642e3d923eb22c58d11
isiss123 <abcecebab12@gmail.com>
2022-10-02 17:26:56 +0400

    ASP-Net Bolum-14 ( ApiServisin projede istifadesi ) (Webapi)

--- Asp-Net/Main/Controllers/HomeController.cs
+++ Asp-Net/Main/Controllers/HomeController.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Main.Business.Abstract;
 using Main.Data.Abstract;
+using Main.Entity;
 using Main.Identity;
 using Main.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
 namespace Yoxlama.Controllers
 {
     public class HomeController : Controller
@@ -23,6 +26,19 @@ namespace Yoxlama.Controllers
                 Products = products
             };
             return View(ProductView);
+        }
+        public async Task<IActionResult> GetProducts()
+        {
+            var productView = new ProductListViewModel();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7235/api/products"))
+                {
+                    var apiResponse = await response.Content.ReadAsStringAsync();
+                    productView.Products = JsonConvert.DeserializeObject<List<Product>>(apiResponse);
+                }
+            }
+            return View(productView);
         }
         public IActionResult About(){
             return View();
